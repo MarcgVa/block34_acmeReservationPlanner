94edc6bca55c4e21be4e3a18d0f3cf4d670cfbc6
Marc Grupe <marc.grupe@gmail.com>
2025-04-21 18:20:57 -0400

    block34 completed

---
+++ common/common.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: process.env.DB_ADMIN,
+  password: process.env.DB_PASSWORD,
+  host: process.env.HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DATABASE,
+});
+
+module.exports = pool;
